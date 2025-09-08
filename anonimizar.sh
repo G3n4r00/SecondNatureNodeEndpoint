d1c7d67b947ac535f93355b4de55156ae5c1c3ea
G3n4r0 <rm551986@fiap.com.br>
2025-09-08 10:39:22 -0300

    sh

--- anonimizar.sh
+++ anonimizar.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
-
 IMAGE="nouchka/sqlite3"
-
-
 DB="/var/lib/sqlite/mydb.sqlite"
 
 SQL_CMD="
 UPDATE usuarios
 SET cpf = '******' || substr(cpf, 7, 3),
     sobrenome = substr(sobrenome, 1, 1) || '*****',
-    email = substr(email,1,1) || '*****' || substr(email, instr(email,'@'), length(email));
+    email = substr(email,1,1) || '*****' || substr(email, instr(email,'@'), length(email)),
     celular = '*******' || substr(celular, -4);
 "
 
-# Roda o container, monta o volume, executa o SQL e remove o container após execução
 docker run --rm -v $(dirname $DB):/db $IMAGE sqlite3 /db/$(basename $DB) "$SQL_CMD"
