356e254cc6bdf1dc2734b283909cb92512e9685f
G3n4r0 <rm551986@fiap.com.br>
2025-10-05 13:09:28 -0300

    changes on eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps: [{
     name: "betware-api",
-    script: "index.js",
-    user: "betwareservices",    // The user PM2 run the app as
-    group: "betwareservices",   // The group PM2 run the app as
+    script: "/srv/betware-api/index.js", 
+    cwd: "/srv/betware-api/",             // Set the current working directory
+    user: "betwareservices",
+    group: "betwareservices",
   }]
 };
-
-// --- IGNORE ---
